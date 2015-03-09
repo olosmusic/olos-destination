74869a519ee8fb70f1432bf70332335b874b8f90
therewasaguy <jason.sigal@gmail.com>
2015-03-09 04:39:20 -0400

    named inputs

--- olos-destination.js
+++ olos-destination.js
@@ -10,70 +10,30 @@
     rootfolder: '../olos-destination/',
 
     // handle i/o
-    input: null,
-    input2: null,
-    inputCount: 2,
-    outputCount: 0,
-    inputs: [],
-
-    // ports: {
-    //   'in' : [
-    //     {
-    //       'sources': [],
-    //       'destination': []
-    //     },
-    //     {
-    //       'sources': [],
-    //       'destination': []
-    //     }
-    //   ],
-    //   'out': []
-    // },
+    inputLeft: null,
+    inputRight: null,
+    output: null,
 
     ready: function() {
       var self = this;
       self._audioContext = audioContext;
-      self.input = self.output = self._audioContext.createGain();
-      self.input.connect(self._audioContext.destination);
-      console.log(self.ports);
+
+      // for now, both L + R are the same
+      self.inputLeft = self.inputRight = self.output = self._audioContext.createGain();
+      self.inputLeft.connect(self._audioContext.destination);
     },
-
-    // inputChanged: function() {
-    //   var self = this;
-    //   if (self.input instanceof AudioNode) {
-    //     self.input.connect(self.gainNode);
-    //   }
-    //   else if (self.input) {
-    //     console.log('not an audio node');
-    //   } else {
-
-    //   }
-    // },
-
-    // TO DO: when an input is removed, remove it from ports
-    // inputremoveChanged: function() {
-
-    // }
 
     _toggleStart: function() {
       if(!this.on) {
-        this.input.connect(this._audioContext.destination);
+        this.inputLeft.connect(this._audioContext.destination);
       }
       else {
-        this.input.disconnect();
+        this.inputLeft.disconnect();
       }
       this.on = !this.on;
     },
 
-    setOutput: function(obj) {
-      var self = this;
-      self._osc.connect(obj);
-      self._output = obj;
-    },
-
     _init: function() {
-      var self = this;
-
     },
 
     publicAudio: function() {
